1457fef0298634a6bba4b40569f965dde8691977
Oliver Elbert <oliver.elbert36@gmail.com>
2024-07-09 14:38:38 -0400

    adding sfc_ocean, moving lsm to a separate branch

---
+++ pySHiELD/functions/physics_functions.py
@@ -0,0 +1,83 @@
+import ndsl.constants as constants
+import pySHiELD.constants as physcons
+
+from gt4py.cartesian import gtscript
+from gt4py.cartesian.gtscript import exp, min, max, floor
+
+@gtscript.function
+def fpvs(t):
+    """
+    Adapted from a lookup table version in Fortran for use in GT4Py
+    """
+    nxpvs = 7501.0
+    xmin = 180.0
+    xmax = 330.0
+    xinc = (xmax - xmin) / (nxpvs - 1)
+    c2xpvs = 1. / xinc
+    c1xpvs = 1. - xmin * c2xpvs
+    xj = min(max(c1xpvs + c2xpvs * t, 1.0), nxpvs)
+    jx = min(xj, nxpvs - 1.0)
+    tx = floor(jx)
+    fpvs = fpvsx(jx - 1) + (xj - jx) * (fpvsx(jx) - fpvsx(jx - 1))
+
+@gtscript.function
+def fpvsx(t):
+    """
+    Computes saturation water vapor pressure, adapted from Fortran:
+    ! Subprogram: fpvsx        Compute saturation vapor pressure
+    !   Author: N Phillips            w/NMC2X2   Date: 30 dec 82
+    !
+    ! Abstract: Exactly compute saturation vapor pressure from temperature.
+    !   The saturation vapor pressure over either liquid and ice is computed
+    !   over liquid for temperatures above the triple point,
+    !   over ice for temperatures 20 degress below the triple point,
+    !   and a linear combination of the two for temperatures in between.
+    !   The water model assumes a perfect gas, constant specific heats
+    !   for gas, liquid and ice, and neglects the volume of the condensate.
+    !   The model does account for the variation of the latent heat
+    !   of condensation and sublimation with temperature.
+    !   The Clausius-Clapeyron equation is integrated from the triple point
+    !   to get the formula
+    !       pvsl=con_psat*(tr**xa)*exp(xb*(1.-tr))
+    !   where tr is ttp/t and other values are physical constants.
+    !   The reference for this computation is Emanuel(1994), pages 116-117.
+    !   This function should be expanded inline in the calling routine.
+    !
+    ! Program History Log:
+    !   91-05-07  Iredell             made into inlinable function
+    !   94-12-30  Iredell             exact computation
+    ! 1999-03-01  Iredell             f90 module
+    ! 2001-02-26  Iredell             ice phase
+    ! 2024-07-09  Oelbert             Python version
+    !
+    ! Usage:   pvs=fpvsx(t)
+    !
+    !   Input argument list:
+    !     t          Real(krealfp) temperature in Kelvin
+    !
+    !   Output argument list:
+    !     fpvsx      Real(krealfp) saturation vapor pressure in Pascals
+    """
+    dldtl = physcons.CPVAP - physcons.CPH2O1
+    xponal = -dldtl / constants.RVGAS
+    xponbl = -dldtl / constants.RVGAS + constants.HLV / (
+        constants.RVGAS * constants.TICE
+    )
+    dldti = physcons.CPVAP - physcons.CPICE
+    heati = constants.HLV + constants.HLF
+    xponai = -dldti / constants.RVGAS
+    xponbi = -dldti / constants.RVGAS + heati / (
+        constants.RVGAS * constants.TICE
+    )
+    tr = constants.TICE / t
+    tice2 = constants.TICE - 20.0
+    if t > constants.TICE:
+        fpvsx = physcons.PSAT * (tr**xponal) * exp(xponbl * (1. - tr))
+    elif t < tice2:
+        fpvsx = physcons.PSAT * (tr**xponai) * exp(xponbi * (1. - tr))
+    else:
+        w = (t - tice2) / (constants.TICE - tice2)
+        pvl = physcons.PSAT * (tr**xponal) * exp(xponbl * (1. - tr))
+        pvi = physcons.PSAT * (tr**xponai) * exp(xponbi * (1. - tr))
+        fpvsx = w * pvl + (1. - w) * pvi
+    return fpvsx
---
+++ pySHiELD/stencils/surface/sfc_ocean.py
@@ -0,0 +1,128 @@
+from gt4py.cartesian import gtscript
+from gt4py.cartesian.gtscript import FORWARD, PARALLEL, computation, exp, interval, log, sqrt
+
+import ndsl.constants as constants
+import pySHiELD.constants as physcons
+from ndsl.constants import X_DIM, Y_DIM, Z_DIM
+
+# from pace.dsl.dace.orchestration import orchestrate
+from ndsl.dsl.stencil import StencilFactory
+from ndsl.dsl.typing import (
+    Bool,
+    BoolFieldIJ,
+    Float,
+    FloatField,
+    FloatFieldIJ,
+    IntFieldIJ,
+)
+from ndsl.initialization.allocator import QuantityFactory
+from ndsl.stencils.basic_operations import sign
+from pySHiELD._config import COND_DIM, SurfaceConfig, TRACER_DIM
+from pySHiELD.functions.physics_functions import fpvs, fpvsx
+
+
+def sfc_ocean(
+    ps: FloatFieldIJ,
+    u1: FloatFieldIJ,
+    v1: FloatFieldIJ,
+    t1: FloatFieldIJ,
+    q1: FloatFieldIJ,
+    tskin: FloatFieldIJ,
+    cm: FloatFieldIJ,
+    ch: FloatFieldIJ,
+    prsl1: FloatFieldIJ,
+    prslki: FloatFieldIJ,
+    ddvel: FloatFieldIJ,
+    qsurf: FloatFieldIJ,
+    cmm: FloatFieldIJ,
+    chh: FloatFieldIJ,
+    gflux: FloatFieldIJ,
+    evap: FloatFieldIJ,
+    hflx: FloatFieldIJ,
+    ep: FloatFieldIJ,
+    islimsk: IntFieldIJ,
+    flag_iter: BoolFieldIJ,
+):
+    with computation(PARALLEL), interval(0, 1):
+        if (islimsk == 0) and (flag_iter):
+            wind = max(sqrt(u1**2 + v1**2) + max(0., min(ddvel, 30)), 1.0)
+            q0 = max(q1, 1.e-8)
+            rho = prsl1 / (constants.RDGAS * t1 * (1.0 + constants.ZVIR * q0))
+
+            qss = fpvsx(tskin)
+            qss = constants.EPS * qss / (ps + (constants.EPS - 1.) * qss)
+
+            evap = 0.0
+            hflx = 0.0
+            ep = 0.0
+            gflux = 0.0
+
+            # rcp  = rho cp ch v
+            rch = rho * constants.CP_AIR * ch * wind
+            cmm = cm * wind
+            chh = rho * ch * wind
+
+            # sensible and latent heat flux over open water:
+            hflx = rch * (tskin - t1 * prslki)
+
+            evap = physcons.HOCP * rch * (qss - q0)
+            qsurf = qss
+
+            tem = 1.0 / rho
+            hflx = hflx * tem / constants.CP
+            evap = evap * tem / constants.HLV
+
+class SFC_Ocean:
+    def __init__(self, stencil_factory: StencilFactory,):
+        grid_indexing = stencil_factory.grid_indexing
+        self._sfc_diff = stencil_factory.from_origin_domain(
+            sfc_ocean,
+            origin=grid_indexing.origin_compute(),
+            domain=grid_indexing.domain_compute(),
+        )
+
+    def __call__(
+        self,
+        ps: FloatFieldIJ,
+        u1: FloatFieldIJ,
+        v1: FloatFieldIJ,
+        t1: FloatFieldIJ,
+        q1: FloatFieldIJ,
+        tskin: FloatFieldIJ,
+        cm: FloatFieldIJ,
+        ch: FloatFieldIJ,
+        prsl1: FloatFieldIJ,
+        prslki: FloatFieldIJ,
+        ddvel: FloatFieldIJ,
+        qsurf: FloatFieldIJ,
+        cmm: FloatFieldIJ,
+        chh: FloatFieldIJ,
+        gflux: FloatFieldIJ,
+        evap: FloatFieldIJ,
+        hflx: FloatFieldIJ,
+        ep: FloatFieldIJ,
+        islimsk: IntFieldIJ,
+        flag_iter: BoolFieldIJ,
+    ):
+        self._sfc_diff(
+            ps,
+            u1,
+            v1,
+            t1,
+            q1,
+            tskin,
+            cm,
+            ch,
+            prsl1,
+            prslki,
+            ddvel,
+            qsurf,
+            cmm,
+            chh,
+            gflux,
+            evap,
+            hflx,
+            ep,
+            islimsk,
+            flag_iter,
+        )
