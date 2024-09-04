d8c750850350b065a80c006cd1838ea13f350c28
Oliver Elbert <oliver.elbert36@gmail.com>
2024-09-04 10:20:53 -0400

    updating test

--- tests/savepoint/translate/translate_sfc_ocean.py
+++ tests/savepoint/translate/translate_sfc_ocean.py
@@ -2,8 +2,8 @@ from ndsl import Namelist, StencilFactory
 from pySHiELD.stencils.surface.sfc_ocean import SurfaceOcean
 from tests.savepoint.translate.translate_physics import TranslatePhysicsFortranData2Py
 
-
-class TranslateSurfaceExchange(TranslatePhysicsFortranData2Py):
+!$ser data ocean_cmm=Diag%cmm ocean_chh=Diag%chh ocean_gflux=gflx ocean_evap=evap ocean_hflx=hflx
+class TranslateSurfaceOcean(TranslatePhysicsFortranData2Py):
     def __init__(
         self,
         namelist: Namelist,
@@ -11,74 +11,35 @@ class TranslateSurfaceExchange(TranslatePhysicsFortranData2Py):
     ):
         super().__init__(namelist, stencil_factory)
         self.in_vars["data_vars"] = {
-            "qvapor": {"dycore": True},
-            "qliquid": {"dycore": True},
-            "qrain": {"dycore": True},
-            "qsnow": {"dycore": True},
-            "qice": {"dycore": True},
-            "qgraupel": {"dycore": True},
-            "qo3mr": {"dycore": True},
-            "qsgs_tke": {"dycore": True},
-            "qcld": {"dycore": True},
-            "pt": {"dycore": True},
-            "delp": {"dycore": True},
-            "delz": {"dycore": True},
-            "ua": {"dycore": True},
-            "va": {"dycore": True},
-            "w": {"dycore": True},
-            "omga": {"dycore": True},
+            "ps": {"serialname": "ocean_ps", "SHiELD": True},
+            "u1": {"serialname": "ocean_u1", "SHiELD": True},
+            "v1": {"serialname": "ocean_v1", "SHiELD": True},
+            "t1": {"serialname": "ocean_t1", "SHiELD": True},
+            "q1": {"serialname": "ocean_q1", "SHiELD": True},
+            "tskin": {"serialname": "ocean_tskin", "SHiELD": True},
+            "cm": {"serialname": "ocean_cm", "SHiELD": True},
+            "ch": {"serialname": "ocean_ch", "SHiELD": True},
+            "prsl1": {"serialname": "ocean_prsl1", "SHiELD": True},
+            "prslki": {"serialname": "ocean_prslki", "SHiELD": True},
+            "ddvel": {"serialname": "ocean_ddvel", "SHiELD": True},
+            "qsurf": {"serialname": "ocean_qsurf", "SHiELD": True},
+            "cmm": {"serialname": "ocean_cmm", "SHiELD": True},
+            "chh": {"serialname": "ocean_chh", "SHiELD": True},
+            "gflux": {"serialname": "ocean_gflux", "SHiELD": True},
+            "evap": {"serialname": "ocean_evap", "SHiELD": True},
+            "hflx": {"serialname": "ocean_hflx", "SHiELD": True},
+            "ep": {"serialname": "ocean_ep", "SHiELD": True},
+            "islimsk": {"serialname": "ocean_islmsk", "SHiELD": True},
+            "flag_iter": {"serialname": "ocean_flag_iter", "SHiELD": True},
         }
         self.out_vars = {
-            "gt0": {
-                "serialname": "IPD_gt0",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "gu0": {
-                "serialname": "IPD_gu0",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "gv0": {
-                "serialname": "IPD_gv0",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qvapor": {
-                "serialname": "IPD_qvapor",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qliquid": {
-                "serialname": "IPD_qliquid",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qrain": {
-                "serialname": "IPD_rain",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qice": {
-                "serialname": "IPD_qice",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qsnow": {
-                "serialname": "IPD_snow",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qgraupel": {
-                "serialname": "IPD_qgraupel",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
-            "qcld": {
-                "serialname": "IPD_qcld",
-                "kend": namelist.npz - 1,
-                "order": "F",
-            },
+            "qsurf": {"serialname": "ocean_qsurf", "SHiELD": True},
+            "cmm": {"serialname": "ocean_cmm", "SHiELD": True},
+            "chh": {"serialname": "ocean_chh", "SHiELD": True},
+            "gflux": {"serialname": "ocean_gflux", "SHiELD": True},
+            "evap": {"serialname": "ocean_evap", "SHiELD": True},
+            "hflx": {"serialname": "ocean_hflx", "SHiELD": True},
+            "ep": {"serialname": "ocean_ep", "SHiELD": True},
         }
         self.stencil_factory = stencil_factory
 
